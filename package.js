65c5f0ab730632f4c085422982cc521a189ba5b4
zhaoyao91 <zhaoyao91@163.com>
2016-04-13 11:00:24 +0800

    update git

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: 'zhaoyao91:accounts-wechat-mp',
     version: '0.0.1',
     summary: 'meteor accounts package for wechat-mp',
-    git: '',
+    git: 'https://github.com/zhaoyao91/meteor-accounts-wechat-mp',
     documentation: 'README.md'
 });
 
